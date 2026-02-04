e28f753085aa97ec31fcd0bac27329b00dc93df2
Eghosa Ordia <eghodia1004@yahoo.com>
2026-02-04 20:28:50 +0100

    feat: clean up IdleState component by removing unused imports and toast functionality

--- frontend/src/minicomponents/IdleState.jsx
+++ frontend/src/minicomponents/IdleState.jsx
@@ -1,9 +1,7 @@
-import React from "react";
 import { FolderOpen, FolderClosed } from "lucide-react";
 import useTrackStore from "../store/useTrackStore";
 import useStep from "../store/useStep";
 import { ScanAudioFiles } from "../constants/functions";
-import { toast } from "sonner";
 
 const handleScan = async () => {
   try {
@@ -29,15 +27,8 @@ const IdleState = () => {
 
   return (
     <div className="state text-center py-12">
-      <button
-        onClick={() =>
-          toast.success("clicked", {
-            description: "You have clicked me, hahaha",
-          })
-        }
-        className="app-iconbox w-32 h-32 mx-auto mb-8 rounded-3xl bg-white/5 text-primary-green border-2 border-dashed border-white/20 flex items-center justify-center group hover:border-primary-green/50 transition-colors duration-300 cursor-pointer"
-      >
-        <FolderOpen className="lucide-icon group-hover:scale-125 transition-all duration-300" />
+      <button className="app-iconbox w-32 h-32 mx-auto mb-8 rounded-3xl bg-white/5 border-2 border-dashed border-white/20 flex items-center justify-center group hover:border-primary-green/50 transition-colors duration-300 cursor-pointer">
+        <FolderOpen className="size-12 text-dark-foreground group-hover:scale-125 group-hover:text-primary-green transition-all duration-300" />
       </button>
 
       <p className="text-dark-foreground mb-8">
@@ -49,7 +40,7 @@ const IdleState = () => {
         id="pickFolderBtn"
         className="inline-flex items-center justify-center gap-4 whitespace-nowrap text-sm transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&amp;_svg]:pointer-events-none [&amp;_svg]:size-4 [&amp;_svg]:shrink-0 shadow btn-primary bg-primary-grborder-primary-green hover:bg-[#1abc54] text-black font-bold px-8 py-3 h-auto rounded-full"
       >
-        <FolderClosed />
+        <FolderClosed className="lucide-icon" />
         Select Folder
       </button>
     </div>
